0e835a21a0b3b2e20f6076e90109cfc34d1558db
TANVIR <mailhossain2013@gmail.com>
2021-12-07 19:40:13 +0600

    3rd Commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-console.log("HI THANVIR")
-let mname = "Tanvir hossain";
+let mname = "Tanvir";
 console.log("My name is +" + mname);
-const names = ["Farhana", "Hossain"]
+const names = ["Farhana", "Hossain", "Shafinaz"]
 let array= names.map( val => console.log(val));
+console.log("WE WILL MAKE THE CHANGE AGAIN");
